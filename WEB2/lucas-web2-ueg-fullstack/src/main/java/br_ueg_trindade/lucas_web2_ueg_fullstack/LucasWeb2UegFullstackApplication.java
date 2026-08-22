0233d8922e7a0d1826b1963d1eadcf9868f09701
lucaseduardo-cas <lucas.alfra@gmail.com>
2026-08-22 13:04:35 -0300

    Estruturando repositorio em pastas WEB1 e WEB2

---
+++ WEB2/lucas-web2-ueg-fullstack/src/main/java/br_ueg_trindade/lucas_web2_ueg_fullstack/LucasWeb2UegFullstackApplication.java
@@ -0,0 +1,13 @@
+package br_ueg_trindade.lucas_web2_ueg_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucasWeb2UegFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucasWeb2UegFullstackApplication.class, args);
+	}
+
+}
---
+++ WEB2/lucas-web2-ueg-fullstack/src/test/java/br_ueg_trindade/lucas_web2_ueg_fullstack/LucasWeb2UegFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br_ueg_trindade.lucas_web2_ueg_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucasWeb2UegFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
